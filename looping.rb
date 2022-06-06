c2bc2ed632fbc14a70cc54b320906005b2abab89
annomao <omaoannkerubo@gmail.com>
2022-06-07 00:52:10 +0300

    commit tree 05e5e5b44c0713938e4819935c5454195072d8aa

--- looping.rb
+++ looping.rb
@@ -1,5 +1,13 @@
 def happy_new_year
   # your code here
+  num = 10
+  while num > 0
+    puts num
+    num -= 1
+    if num < 1
+    puts "Happy New Year!"
+   end
+  end
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +25,17 @@ end
 
 def fizzbuzz_printer
   # your code here
+  (1..100).each do |num|
+    value = fizzbuzz(num)
+    puts value
+  end
 end
 
 def reverse_string(str)
   # your code here
+  reversed_string = ""
+  str.length.times do |j|
+    reversed_string = str[j] + reversed_string 
+  end
+  reversed_string
 end
